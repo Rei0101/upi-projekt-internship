5c3acab0ef593fa367e4b598bd8cb203bb906d8f
Rei0101 <62380465+Rei0101@users.noreply.github.com>
2024-12-27 17:53:13 +0100

    Početna struktura App.jsx (dodan react-router-dom)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,25 @@
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import './App.css'; 
+import Login from './pages/Login';
+/* import Header from './components/Header';
+import Footer from './components/Footer'; */
+//? u slučaju da želite header i footer komponente
 
 function App() {
-
   return (
-    <>
-      Frontend
-    </>
-  )
+    <Router>
+      {/* <Header />  */}
+      
+      <div className="main-content">
+        <Routes>
+          <Route path="/" element={<Login />} />
+        </Routes>
+      </div>
+      
+      {/* <Footer /> */}
+    </Router>
+  );
 }
 
-export default App
+export default App;
+
