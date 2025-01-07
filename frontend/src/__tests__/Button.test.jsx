a7a9eac21c7539be598164a2050f466c311625a0
dkrstic12 <reikrstic1010@gmail.com>
2025-01-07 20:26:15 +0100

    Implementirana logika za testove (i dodani neki testovi)

---
+++ frontend/src/__tests__/Button.test.jsx
@@ -0,0 +1,40 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import { describe, test, expect, vi } from "vitest";
+import Button from "/src/components/Button.jsx"; // Adjust the import path as needed
+
+describe("Button komponenta", () => {
+  test("pravilno se render-a s pravim props-ima", () => {
+    render(<Button type="button" text="Click Me" />);
+
+    const buttonElement = screen.getByText("Click Me");
+    expect(buttonElement).toBeInTheDocument();
+
+    expect(buttonElement).toHaveAttribute("type", "button");
+  });
+
+  test("postavi se className kad je zadan", () => {
+    render(<Button type="button" text="Click Me" className="primary-btn" />);
+
+    const buttonElement = screen.getByText("Click Me");
+    expect(buttonElement).toHaveClass("primary-btn");
+  });
+
+  test("na klik se pozove \"onClick\"", () => {
+    const handleClick = vi.fn();
+
+    render(<Button type="button" text="Click Me" onClick={handleClick} />);
+
+    const buttonElement = screen.getByText("Click Me");
+
+    fireEvent.click(buttonElement);
+
+    expect(handleClick).toHaveBeenCalledTimes(1);
+  });
+
+  test("ne render-a se className ako nije zadan", () => {
+    render(<Button type="button" text="Click Me" />);
+
+    const buttonElement = screen.getByText("Click Me");
+    expect(buttonElement).not.toHaveClass();
+  });
+});
--- frontend/src/pages/Raspored.jsx
+++ frontend/src/pages/Raspored.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import "./raspored.css";
 
 function Raspored() {
