01c0fa4fb0af684d9ef61427576dcbacdf524888
daitoy <mbajic1@pmfst.hr>
2025-01-22 17:31:01 +0100

    dodan app.test.jsx i testovi

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,13 @@
+/// <reference types="vitest" />
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
-    environment: 'jsdom',
     globals: true,
-    setupFiles: ['./src/__tests__/setup.js'],
-    coverage: {
-      reporter: ['text', 'html', 'lcov']
-    },
+    environment: 'jsdom',
+    setupFiles: './src/__tests__/setup.js',
   },
 });
