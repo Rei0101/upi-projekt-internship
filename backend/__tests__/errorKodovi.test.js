bffb35135116588d10b7f15d7b3b363c76d7d315
daitoy <mbajic1@pmfst.hr>
2025-01-23 16:15:40 +0100

    dodano jos testova u errorkodovi.test

--- backend/__tests__/errorKodovi.test.js
+++ backend/__tests__/errorKodovi.test.js
@@ -28,7 +28,24 @@ import jest from 'jest-mock';
         message: "Zadana tablica nije dozvoljena.",
       });
     });
-    
-  
+    test('INVALID_EMAIL treba vratiti status 400 s ispravnim odgovorom', () => {
+      INVALID_EMAIL(res);
+      expect(res.status).toHaveBeenCalledWith(400);
+      expect(res.json).toHaveBeenCalledWith({
+        success: false,
+        errorCode: "INVALID_EMAIL",
+        message: "E-mail nije poslan.",
+      });
+    });
+    test('NOT_AUTHORIZED treba vratiti status 401 s ispravnim odgovorom', () => {
+      NOT_AUTHORIZED(res);
+      expect(res.status).toHaveBeenCalledWith(401);
+      expect(res.json).toHaveBeenCalledWith({
+        success: false,
+        errorCode: "NOT_AUTHORIZED",
+        message: "Dogodila se greška pri provjeri valjanosti.",
+      });
+    });
+
 
   });
