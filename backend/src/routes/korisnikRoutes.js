59141224979901590e46b785fbcd766b591d5fcc
TheGargamel <ababicsus@pmfst.hr>
2025-01-23 13:33:40 +0100

    Nadodane razmjene među studentima u backendu

--- backend/src/routes/korisnikRoutes.js
+++ backend/src/routes/korisnikRoutes.js
@@ -3,8 +3,11 @@ import {
   loginUser,
   getTimetable,
   getAllGroups,
-  getToDo,
-  updateToDo
+  updateToDo,
+  changeGroup,
+  sendExchangeRequest,
+  getExchangeRequests,
+  handleExchangeResponse
 } from "../viewModels/korisnikViewModel.js";
 import userTypeMiddleWare from "../utils/userTypeMiddleWare.js";
 
@@ -13,7 +16,11 @@ const router = express.Router();
 router.post("/login", userTypeMiddleWare, loginUser);
 router.post("/raspored", userTypeMiddleWare, getTimetable);
 router.post("/sve-grupe/:id?", userTypeMiddleWare, getAllGroups);
-router.get("/todo", userTypeMiddleWare, getToDo);
 router.put("/novi-todo", userTypeMiddleWare, updateToDo);
+router.patch("/promjena-grupe", changeGroup);
+router.post("/zahtjev-razmjene",sendExchangeRequest);
+router.post("/dobavi-zahtjev",getExchangeRequests);
+router.post("/obradi-zahtjev", handleExchangeResponse);
+
 
 export default router;
