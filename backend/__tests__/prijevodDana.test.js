351d896df679f3934a98a12fac1d59a6660ca7e9
daitoy <mbajic1@pmfst.hr>
2025-01-26 00:00:50 +0100

    dodani ostali testovi za prijevoddana

--- backend/__tests__/prijevodDana.test.js
+++ backend/__tests__/prijevodDana.test.js
@@ -27,6 +27,32 @@ describe('Prevoditelj dana', () => {
       expect(prevoditelj.naHrvatski('InvalidDay')).toBe('Netočan dan');
     });
   });
+  
+  describe('naEngleski', () => {
+    test('prevodi Ponedjeljak u Monday', () => {
+      expect(prevoditelj.naEngleski('Ponedjeljak')).toBe('Monday');
+    });
+
+    test('prevodi sve dane u tjednu na engleski', () => {
+      const dani = {
+        'Ponedjeljak': 'Monday',
+        'Utorak': 'Tuesday',
+        'Srijeda': 'Wednesday',
+        'Četvrtak': 'Thursday',
+        'Petak': 'Friday',
+        'Subota': 'Saturday',
+        'Nedjelja': 'Sunday'
+      };
+
+      Object.entries(dani).forEach(([hrvatski, engleski]) => {
+        expect(prevoditelj.naEngleski(hrvatski)).toBe(engleski);
+      });
+    });
+
+    test('vraća "Netočan dan" za nepostojeći dan', () => {
+      expect(prevoditelj.naEngleski('NepostojeciDan')).toBe('Netočan dan');
+    });
+  });
 
 
   
